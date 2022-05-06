6efe87be65845ba8b6fab0990747e9ad83feff52
j-ahmedov <j.ahmedov.m@gmail.com>
2022-05-06 20:16:15 +0500

    Changed files

--- iTechDaily/settings.py
+++ iTechDaily/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$s!)8c8%9nnntw6dg**109zd%0n+&(m&awstzr$f-a=wy%!sdf'
+SECRET_KEY = 'django-insecure-$s!)8c8%9nnntw6dg**109zd%0n+&(m&awstzr$f-a=wy%!^m9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -131,8 +131,8 @@ django_heroku.settings(locals())
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'example@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_USER = 'j.ahmedov.m99@gmail.com'
+EMAIL_HOST_PASSWORD = '1sforchann3ls2019'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 

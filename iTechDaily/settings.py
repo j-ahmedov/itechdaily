579c22a9088662c31c6675ac1186ea38bb299413
j-ahmedov <j.ahmedov.m@gmail.com>
2022-05-06 17:35:37 +0500

    Changes in 06.05

--- iTechDaily/settings.py
+++ iTechDaily/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$s!)8c8%9nnntw6dg**109zd%0n+&(m&awstzr$f-a=wy%!^m9'
+SECRET_KEY = 'django-insecure-$s!)8c8%9nnntw6dg**109zd%0n+&(m&awstzr$f-a=wyda5665656565'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
